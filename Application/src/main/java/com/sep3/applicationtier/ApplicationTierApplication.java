0dacf5d058c2e802ab5a650c872c765c57fa8819
Andrei Ioanas <ioanasandrei13@gmail.com>
2022-04-17 13:38:48 +0200

    -app commit

---
+++ Application/src/main/java/com/sep3/applicationtier/ApplicationTierApplication.java
@@ -0,0 +1,13 @@
+package com.sep3.applicationtier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTierApplication.class, args);
+    }
+
+}
---
+++ Application/src/test/java/com/sep3/applicationtier/ApplicationTierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep3.applicationtier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
