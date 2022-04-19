2e04581527872cb1c5828802f9cca64539a927ee
antom <antomax.9960@gmail.com>
2022-04-19 13:53:48 +0200

    -Application working client server connection

--- Application/src/main/java/RESTClientConnection/OrderController.java
+++ Application/src/main/java/RESTClientConnection/OrderController.java
@@ -7,9 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/data")
 public class OrderController {
 
-    @GetMapping("/data")
+    @GetMapping("/order")
     public ResponseEntity<Order> get() {
         Order order = new Order(1,1,"Received",false);
         return new ResponseEntity<Order>(order, HttpStatus.OK);
